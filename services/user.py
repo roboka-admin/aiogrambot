4738ae93ea55ca9f4bc227e3b4402551b6e8fa80
roboka-admin <robokamhp@gmail.com>
2026-08-18 21:34:05 +0330

    Add get_user operation to UserService

--- services/user.py
+++ services/user.py
@@ -1,3 +1,5 @@
+from exceptions.user import UserNotFoundError
+from models.user import User
 from repositories.interfaces.user import IUserRepository
 
 
@@ -14,3 +16,12 @@ class UserService:
     async def exists(self, telegram_id: int) -> bool:
         """Check whether a Telegram user is registered."""
         return await self._user_repository.exists(telegram_id)
+
+    async def get_user(self, telegram_id: int) -> User:
+        """Return a registered user or raise if the user does not exist."""
+        user = await self._user_repository.get_by_telegram_id(telegram_id)
+
+        if user is None:
+            raise UserNotFoundError()
+
+        return user
