f625cac6383ced8bfab86e2565c19ea22656e4aa
roboka-admin <robokamhp@gmail.com>
2026-08-19 21:57:02 +0330

    Add admin user action callback data

--- callbacks/admin.py
+++ callbacks/admin.py
@@ -7,3 +7,8 @@ class AdminUsersCallback(CallbackData, prefix="admin_users"):
 
 class AdminUserCallback(CallbackData, prefix="admin_user"):
     telegram_id: int
+
+
+class AdminUserActionCallback(CallbackData, prefix="admin_user_action"):
+    action: str
+    telegram_id: int
