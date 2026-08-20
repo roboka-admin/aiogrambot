53fce6250a5a742dd8fd308828223526f829e46c
roboka-admin <robokamhp@gmail.com>
2026-08-20 11:34:23 +0330

    Add pagination data to support callbacks

--- callbacks/admin_support.py
+++ callbacks/admin_support.py
@@ -3,10 +3,13 @@ from aiogram.filters.callback_data import CallbackData
 
 class AdminSupportListCallback(CallbackData, prefix="admin_support_list"):
     status: str
+    page: int
 
 
 class AdminSupportTicketCallback(CallbackData, prefix="admin_support_ticket"):
     ticket_id: int
+    status: str
+    page: int
 
 
 class AdminSupportBackCallback(CallbackData, prefix="admin_support_back"):
