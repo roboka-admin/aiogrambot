03ac74adc1592e00523fbb50ce53babae7264a89
roboka-admin <robokamhp@gmail.com>
2026-08-20 10:51:47 +0330

    Add support message validator

---
+++ validators/support.py
@@ -0,0 +1,36 @@
+from typing import Final
+
+from aiogram.enums import ContentType
+from aiogram.types import Message
+
+
+MAX_SUPPORT_TEXT_LENGTH: Final[int] = 2000
+
+SUPPORTED_CONTENT_TYPES: Final[frozenset[ContentType]] = frozenset(
+    {
+        ContentType.TEXT,
+        ContentType.PHOTO,
+        ContentType.VIDEO,
+        ContentType.DOCUMENT,
+        ContentType.AUDIO,
+        ContentType.VOICE,
+        ContentType.STICKER,
+        ContentType.ANIMATION,
+        ContentType.VIDEO_NOTE,
+    }
+)
+
+
+def validate_support_message(message: Message) -> bool:
+    """Validate whether a Telegram message is allowed for support."""
+
+    if message.content_type not in SUPPORTED_CONTENT_TYPES:
+        return False
+
+    if message.text is not None and len(message.text) > MAX_SUPPORT_TEXT_LENGTH:
+        return False
+
+    if message.caption is not None and len(message.caption) > MAX_SUPPORT_TEXT_LENGTH:
+        return False
+
+    return True
