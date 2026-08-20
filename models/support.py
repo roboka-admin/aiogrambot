f41a8223cc74f9e400e98c7801ff0f96e71f94e6
roboka-admin <robokamhp@gmail.com>
2026-08-20 11:50:39 +0330

    Add support user summary model

--- models/support.py
+++ models/support.py
@@ -3,17 +3,19 @@ from enum import Enum
 
 
 class SupportStatus(str, Enum):
-    """Represents the current status of a support ticket."""
-
     OPEN = "open"
     CLOSED = "closed"
 
 
 @dataclass(slots=True)
 class SupportTicket:
-    """Domain model representing a user support ticket."""
-
     id: int | None
     user_telegram_id: int
     message: str
     status: SupportStatus = SupportStatus.OPEN
+
+
+@dataclass(slots=True)
+class SupportUserSummary:
+    user_telegram_id: int
+    ticket_count: int
