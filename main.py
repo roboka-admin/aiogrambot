9f23e1de39e95c08257cde43410f9fae6c7bfd29
roboka-admin <robokamhp@gmail.com>
2026-08-22 23:40:37 +0330

    Wire support settings router into application

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from config import BOT_TOKEN, DATABASE_URL
 from core.database import Database
 from handlers.admin import router as admin_router
 from handlers.admin_support import router as admin_support_router
+from handlers.admin_support_settings import router as admin_support_settings_router
 from handlers.edit_profile import router as edit_profile_router
 from handlers.profile import router as profile_router
 from handlers.register import router as register_router
@@ -43,6 +44,7 @@ async def main() -> None:
     dp.include_router(support_router)
     dp.include_router(edit_profile_router)
     dp.include_router(admin_router)
+    dp.include_router(admin_support_settings_router)
     dp.include_router(admin_support_router)
 
     try:
