9a23328c8ac46fd1edf605f27eb43ad0c9208d72
roboka-admin <robokamhp@gmail.com>
2026-08-26 12:05:17 +0330

    Move blocked users entry into users section

--- keyboards/admin.py
+++ keyboards/admin.py
@@ -3,13 +3,8 @@ from aiogram.types import KeyboardButton, ReplyKeyboardMarkup
 
 admin_menu = ReplyKeyboardMarkup(
     keyboard=[
-        [
-            KeyboardButton(text="👤 کاربران"),
-            KeyboardButton(text="🚫 کاربران مسدود"),
-        ],
-        [
-            KeyboardButton(text="📩 پشتیبانی"),
-        ],
+        [KeyboardButton(text="👤 کاربران")],
+        [KeyboardButton(text="📩 پشتیبانی")],
         [
             KeyboardButton(text="📊 آمار"),
             KeyboardButton(text="📢 ارسال همگانی"),
